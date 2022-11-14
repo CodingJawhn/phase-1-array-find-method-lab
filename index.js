fe08e383ec9fe7d2652cf58ce95cfda7216c9b82
John Yang <john.yang8097@gmail.com>
2022-11-13 20:28:16 -0500

    commit tree 0ab63b7c49281712850f50ab6686e79855690159

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    let win = record.find(function(record) {
+        return record.result === 'W'
+    })
+    return win ? win.year : undefined
+}
+
